f6e517b5b51225c3e900740d72d49556cb2b8762
SarahOtmane <sarahotmane02@gmail.com>
2025-02-21 22:33:23 +0100

    supprimer le préfixe global 'api' dans le fichier main.ts

--- api/src/main.ts
+++ api/src/main.ts
@@ -4,7 +4,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   try {
     const app = await NestFactory.create(AppModule);
-    app.setGlobalPrefix('api');
     await app.listen(3000);
     console.log('Application is running on: http://localhost:3000');
   } catch (error) {
